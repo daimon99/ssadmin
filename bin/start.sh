771dd83198cd8e188b5310d3b2592ed482786790
Daimon@imac <daijian1@qq.com>
2016-11-09 01:38:42 +0800

    fix bugs

--- bin/start.sh
+++ bin/start.sh
@@ -2,4 +2,11 @@
 
 root=`dirname $0`/..
 source $root/venv/bin/activate
-lsof -i
+a=`lsof -i:9028`
+if [ -z "$a" ];then
+  echo "重启服务"
+  cd $root/src
+  nohup python manage.py runserver 9028 >> nohup.out &
+else
+  echo "服务已经在运行中，不需要再启动"
+fi
