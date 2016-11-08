cd0a17fe616025758c05c4e98fbeb1b850a529c8
daimon@air <daijian1@qq.com>
2016-11-08 19:31:58 +0800

    完善脚本

--- fabfile.py
+++ fabfile.py
@@ -1,13 +1,8 @@
 # coding:utf8
 
 import os
-import random
-import string
 
-import requests
-from fabric.api import env, lcd, local, require
-from fabric.colors import cyan
-from fabric.operations import prompt
+from fabric.api import env, lcd, local
 
 current_dir = os.getcwd()
 env.project_name = 'turing'
@@ -15,24 +10,35 @@ env.branch = 'master'
 
 
 def export_req():
+    """制作python依赖文件"""
     local('pip freeze > requirements.txt')
 
 
 def deploy():
+    """正常部署应用"""
     with lcd('playbook'):
         local('ansible-playbook playbook.yml')
 
 
+def quick():
+    """快速部署应用"""
+    with lcd('playbook'):
+        local('ansible-playbook playbook.yml --tags=quick')
+
+
 def install_roles():
+    """安装ansible roles"""
     with lcd('playbook'):
         local('ansible-galaxy install -r requirements.yml -p ./roles -f')
 
 
 def migrate():
+    """同步模型"""
     with lcd('src'):
         local('python manage.py makemigrations')
         local('python manage.py migrate')
 
 
 def run():
+    """运行应用"""
     local('python src/manage.py runserver 8000')
