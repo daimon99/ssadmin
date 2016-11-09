b0489753b3acb0f68ae5061a5d486a02bf426052
daimon@air <daijian1@qq.com>
2016-11-09 15:01:27 +0800

    add_user

--- src/ssadmin/services.py
+++ src/ssadmin/services.py
@@ -5,10 +5,10 @@ import subprocess
 log = logging.getLogger(__name__)
 
 
-def add_user(port, password):
+def add_user(port, password, limit):
     """创建ss用户"""
     log.debug(u'创建ss用户， port=%s, password=%s', port, password)
-    ret = subprocess.check_output(['/data/prd/ss-bash/ssadmin.sh', 'show'])
+    ret = subprocess.check_output(['/data/prd/ss-bash/ssadmin.sh', 'add', port, password, limit])
     print type(ret), ret
 
 if __name__ == '__main__':
