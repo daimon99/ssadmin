189654c3aac319e3c8cc64e04977a442dac5e923
daimon@air <daijian1@qq.com>
2016-11-09 15:10:29 +0800

    fix bugs

--- src/ssadmin/services.py
+++ src/ssadmin/services.py
@@ -12,9 +12,14 @@ def add_user(port, password, limit):
     """
     port, password, limit = str(port), str(password), str(limit)
     log.debug(u'创建ss用户， port=%s, password=%s', port, password)
-    ret = subprocess.check_output(['/data/prd/ss-bash/ssadmin.sh', 'add', port, password, limit])
-    print ret
+    try:
+        ret = subprocess.check_output(['/data/prd/ss-bash/ssadmin.sh', 'add', port, password, limit])
+        print ret
+    except subprocess.CalledProcessError as e:
+        print e.cmd, e.returncode, e.output
+
 
 if __name__ == '__main__':
     import doctest
+
     doctest.testmod()
