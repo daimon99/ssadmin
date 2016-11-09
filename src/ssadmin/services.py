2375df27a23799b72fe5bde77232284d2ab13f4f
daimon@air <daijian1@qq.com>
2016-11-09 17:11:09 +0800

    add basic scripts

--- src/ssadmin/services.py
+++ src/ssadmin/services.py
@@ -17,7 +17,7 @@ def log_wrapper(func):
             log.info(ret)
             return ret
         except subprocess.CalledProcessError as e:
-            log.error(e)
+            log.error(u'调用出错：%s, %s, %s', e.cmd, e.returncode, e.output)
             return e.returncode
         except OSError as e:
             log.error(e)
@@ -34,7 +34,7 @@ def add(port, password, limit):
     ['/data/prd/ss-bash/ssadmin.sh', 'add', '10001', 'daijian', '1g'] 1 用户已存在!
     """
     port, password, limit = str(port), str(password), str(limit)
-    ret = subprocess.check_output([SS_ADMIN, 'add', port, password, limit])
+    ret = subprocess.check_output([SS_ADMIN, 'add', port, password, limit], stderr=subprocess.STDOUT)
     return ret
 
 
@@ -42,7 +42,7 @@ def add(port, password, limit):
 def clim(port, limit):
     """修改用户流量"""
     port, limit = str(port), str(limit)
-    ret = subprocess.check_output([SS_ADMIN, 'clim', port, limit])
+    ret = subprocess.check_output([SS_ADMIN, 'clim', port, limit], stderr=subprocess.STDOUT)
     return ret
 
 
@@ -50,7 +50,7 @@ def clim(port, limit):
 def rlim(port):
     """用户流量限制置0"""
     port = str(port)
-    ret = subprocess.check_output([SS_ADMIN, 'rlim', port])
+    ret = subprocess.check_output([SS_ADMIN, 'rlim', port], stderr=subprocess.STDOUT)
     return ret
 
 
@@ -58,7 +58,7 @@ def rlim(port):
 def rused(port):
     """用户使用量置0"""
     port = str(port)
-    ret = subprocess.check_output([SS_ADMIN, 'rused', port])
+    ret = subprocess.check_output([SS_ADMIN, 'rused', port], stderr=subprocess.STDOUT)
     return ret
 
 
@@ -66,42 +66,42 @@ def rused(port):
 def change(port, password, limit):
     """修改用户"""
     port, password, limit = str(port), str(password), str(limit)
-    ret = subprocess.check_output([SS_ADMIN, 'change', port, password, limit])
+    ret = subprocess.check_output([SS_ADMIN, 'change', port, password, limit], stderr=subprocess.STDOUT)
     return ret
 
 
 @log_wrapper
 def soft_restart():
     """在不影响现有连接的情况下重启ss服务。用于ss服务参数修改，和手动直接修改配置文件后，重启ss服务。"""
-    ret = subprocess.check_output([SS_ADMIN, 'soft_restart'])
+    ret = subprocess.check_output([SS_ADMIN, 'soft_restart'], stderr=subprocess.STDOUT)
     return ret
 
 
 @log_wrapper
 def restart():
     """重启ss服务。"""
-    ret = subprocess.check_output([SS_ADMIN, 'restart'])
+    ret = subprocess.check_output([SS_ADMIN, 'restart'], stderr=subprocess.STDOUT)
     return ret
 
 
 @log_wrapper
 def start():
     """启动ss服务。"""
-    ret = subprocess.check_output([SS_ADMIN, 'start'])
+    ret = subprocess.check_output([SS_ADMIN, 'start'], stderr=subprocess.STDOUT)
     return ret
 
 
 @log_wrapper
 def stop():
     """停止ss服务。"""
-    ret = subprocess.check_output([SS_ADMIN, 'stop'])
+    ret = subprocess.check_output([SS_ADMIN, 'stop'], stderr=subprocess.STDOUT)
     return ret
 
 
 @log_wrapper
 def show():
     """显示所有用户流量信息"""
-    ret = subprocess.check_output([SS_ADMIN, 'show'])
+    ret = subprocess.check_output([SS_ADMIN, 'show'], stderr=subprocess.STDOUT)
     return ret
 
 
